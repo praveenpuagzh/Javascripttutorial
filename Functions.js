59a6c705472c47ae4cb836aa98620b589f2ae254
praveenpugazh <praveenpugazh84@gmail.com>
2022-08-02 18:10:00 +0530

    updated

--- Functions.js
+++ Functions.js
@@ -1,33 +1,38 @@
 //non-parameterized functions
 function greet() {
-    console.log("Helloworld")
+    console.log("Hello world")
 }
 greet()
 
-function add(){
-console.log(2+3)
+function add() {
+    console.log(2 + 3)
 }
 add()
 
 //named fucntion
-let subraction = function(){
-    console.log(6-4)
+let subraction = function () {
+    console.log(6 - 4)
 }
 subraction()
 
 //paramerterized function
 
-let mul = function(num1,num2){
-
-    console.log(num1*num2)
+function greetuser(name) {
+    console.log("Hello" + " " + name)
 }
-mul(2,2)
+greetuser("praveen")
+
+let mul = function (num1, num2) {
+
+    console.log(num1 * num2)
+}
+mul(2, 2)
 
 
 //arrow function
-let div = (num1,num2) => {
+let div = (num1, num2) => {
 
-    console.log(num1/num2)
+    console.log(num1 / num2)
 }
-div(10,2)
+div(10, 2)
 
